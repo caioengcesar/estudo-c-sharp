c3ba130255ffde2fb1aeb8f484e9d0d37b3fd395
Caio Silva <caio.eng.silva@gmail.com>
2021-11-05 16:35:38 -0300

    Renomeie projeto 3, criei projeto 6

---
+++ ByteBank/06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,75 @@
+﻿//using _05_ByteBank;
+
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        private Cliente _titular;
+        public int agencia;
+        public int numero;
+        private double _saldo = 100;
+
+        public Cliente Titular{get;set;}
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
+
+        public void SetSaldo(double saldo)
+        {
+            if(saldo < 0)
+            {
+                return;
+            }
+            this._saldo = saldo;
+        }
+        public double GetSaldo()
+        {
+            return _saldo;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+}
